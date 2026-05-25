fc07f294261d9b9d319df1474a51c080d5ee65a7
Arsal <arsalkamoka786@gmail.com>
2026-05-25 19:51:25 +0500

    changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,4 @@
-﻿<!-- resources/views/layouts/main.blade.php -->
-
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -1,5 +1,4 @@
-﻿<!-- resources/views/product-details.blade.php -->
-@extends('layouts.main')
+﻿@extends('layouts.main')
 
 @section('title', ($item->meta_title ?? $item->name ?? 'Product Details') . ' | PeytonGhalib')
 @section('meta_description', $item->meta_description ?? \Illuminate\Support\Str::limit(strip_tags($item->description ?? 'Shop ' . ($item->name ?? 'this product') . ' at PeytonGhalib. Quality furniture and home decor at unbeatable prices with fast delivery.'), 155))
