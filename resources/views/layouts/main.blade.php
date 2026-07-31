751dfa6d063abaff087d24e047ece295714b4ac8
Arsal <arsalkamoka786@gmail.com>
2026-07-31 16:12:15 +0500

    cha

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -160,11 +160,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1675737636873475');
+fbq('init', @json(config('services.meta.pixel_id')));
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1675737636873475&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id={{ urlencode(config('services.meta.pixel_id')) }}&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
     </head>
--- resources/views/layouts/no-header.blade.php
+++ resources/views/layouts/no-header.blade.php
@@ -54,6 +54,28 @@
         </script>
         <!-- TikTok Pixel Code End -->
         @include('includes.tiktok-events')
+
+        {{-- These pages are noindex utility screens (login, register, password
+             reset) and carry no conversion event, but the base pixel still has to
+             load: without it Meta sees the session drop off the site at sign-in
+             and attribution for the purchase that follows is broken. --}}
+        <!-- Meta Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', @json(config('services.meta.pixel_id')));
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id={{ urlencode(config('services.meta.pixel_id')) }}&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
     </head>
 
     <body class="dark:bg-title">
